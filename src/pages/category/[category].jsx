b008c3dee92148dc8ab8f86afe8c18acdee0fc17
touseef <touseef.rehman@productbox.dev>
2023-06-24 13:06:51 +0500

    changes: sitemap

--- src/pages/category/[category].jsx
+++ src/pages/category/[category].jsx
@@ -1,10 +1,10 @@
-import { createClient } from "contentful";
-import Layout from "@/components/Layout";
-import safeJsonStringify from "safe-json-stringify";
-import Link from "next/link";
-import Hero from "@/components/Hero";
-import Head from "next/head";
-import { SiteData } from "@/utils/SiteData";
+import { createClient } from 'contentful';
+import Layout from '@/components/Layout';
+import safeJsonStringify from 'safe-json-stringify';
+import Link from 'next/link';
+import Hero from '@/components/Hero';
+import Head from 'next/head';
+import { SiteData } from '@/utils/SiteData';
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
@@ -12,7 +12,7 @@ const client = createClient({
 });
 
 export async function getStaticPaths() {
-  const data = await client.getEntries({ content_type: "category" });
+  const data = await client.getEntries({ content_type: 'category' });
 
   const paths = data.items.map((item) => ({
     params: { category: item.fields.slug },
@@ -26,8 +26,8 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const res = await client.getEntries({
-    content_type: "category",
-    "fields.slug": params.category,
+    content_type: 'category',
+    'fields.slug': params.category,
   });
 
   const stringifiedData = safeJsonStringify(res.items[0]);
@@ -43,47 +43,48 @@ export async function getStaticProps({ params }) {
 
 const Category = ({ category }) => {
   const { siteTitle, siteUrl } = SiteData;
+
   return (
     <Layout>
       <Head>
         <title>{`${category.fields?.categoryName} Articles | ${siteTitle}`}</title>
         <meta
-          property="og:url"
+          property='og:url'
           content={`${siteUrl}/category/${category.fields?.slug}`}
         />
         <meta
-          property="og:description"
+          property='og:description'
           content={category.fields?.description}
         />
-        <meta property="description" content={category.fields?.description} />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content={category.fields?.categoryName} />
-        <meta property="og:site_name" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content={`@${siteTitle}`} />
-        <meta name="twitter:site" content={`@${siteTitle}`} />
+        <meta property='description' content={category.fields?.description} />
+        <meta property='og:locale' content='en_US' />
+        <meta property='og:type' content='article' />
+        <meta property='og:title' content={category.fields?.categoryName} />
+        <meta property='og:site_name' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:creator' content={`@${siteTitle}`} />
+        <meta name='twitter:site' content={`@${siteTitle}`} />
         <link
-          rel="canonical"
+          rel='canonical'
           href={`${siteUrl}/category/${category.fields?.slug}`}
         />
       </Head>
-      <main className="category_main">
+      <main className='category_main'>
         <Hero
           heroTitle={category.fields?.categoryName}
           heroDescription={category.fields?.description}
         />
         <article>
-          <div className="cat_articles">
+          <div className='cat_articles'>
             {!category.fields?.categoryBlogs ? (
-              <div className="under_construction">
+              <div className='under_construction'>
                 <h2>Under Construction...</h2>
               </div>
             ) : (
               category.fields?.categoryBlogs?.map((item) => {
                 return (
                   <Link href={`/blog/${item.fields?.slug}`} key={item.sys?.id}>
-                    <a className="cat_article_link">
+                    <a className='cat_article_link'>
                       {/* <div className="cat_feat_wrapper">
                         <Image
                           src={`https:${item.fields?.featuredImage?.fields?.file?.url}?q=20&fm=jpg&fl=progressive`}
@@ -100,9 +101,9 @@ const Category = ({ category }) => {
                           blurDataURL={`https:${item.fields?.featuredImage?.fields?.file?.url}`}
                         />
                       </div> */}
-                      <div className="cat_feat_content">
-                        <h2 className="cat_feat_title">{item.fields?.title}</h2>
-                        <p className="cat_feat_excerpt">
+                      <div className='cat_feat_content'>
+                        <h2 className='cat_feat_title'>{item.fields?.title}</h2>
+                        <p className='cat_feat_excerpt'>
                           {item.fields?.excerpt}
                         </p>
                       </div>
